3c5aac3617e712be957d17516846ce0b4ffb1f30
Zachary Hancock <zachbhancock@gmail.com>
2022-11-08 10:26:25 -0500

    blah

--- wrapper-run_wmModel_hpcc.sh
+++ wrapper-run_wmModel_hpcc.sh
@@ -13,8 +13,8 @@ finaldir=$storagenode/summary_files #where the models go to die
 model_flavor=wishart #value wishart or cmplnl
 
 #define some values to pass into slim
-vector_of_K_values=( 2.0 5.0 10.0 25.0 )
-vector_of_sigma_values=( 0.5 0.75 1.0 1.25 1.5 2.0 )
+vector_of_K_values=( 25.0 )
+vector_of_sigma_values=( 2.0 )
 
 cpus=2 #number of CPUs to request/use per dataset
 ram_per_cpu=16G #amount of RAM to request/use per CPU
