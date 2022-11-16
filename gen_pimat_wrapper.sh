a711809f2034a916bc83a3d219913af64e8135ea
Zachary Hancock <zachbhancock@gmail.com>
2022-11-16 09:44:43 -0500

    iters

--- gen_pimat_wrapper.sh
+++ gen_pimat_wrapper.sh
@@ -8,11 +8,11 @@ logfilesdir=logfiles_wmModel #name of directory to create and then write log fil
 datesuffix=$(date +%m-%d-%Y.%H)
 outdir=$storagenode/allpimat_wmModel_outputs-$datesuffix #name of directory to create and write all outputs to
 
-n_iterations=9 #number of iterations to run of each identical simulation
+n_iterations=1 #number of iterations to run of each identical simulation
 
 #define some values to pass into slim
-vector_of_K_values=( 25.0 )
-vector_of_sigma_values=( 2.0 )
+vector_of_K_values=( 2.0, 5.0, 10.0, 25.0 )
+vector_of_sigma_values=( 0.5, 0.75, 1.0, 1.25, 1.5, 2.0 )
 
 cpus=2 #number of CPUs to request/use per dataset
 ram_per_cpu=16G #amount of RAM to request/use per CPU
