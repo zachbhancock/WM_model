1d51547d77c33688538d7bf475eb9ee7b36f7466
Rachel Toczydlowski <rhtoczydlowski@gmail.com>
2022-11-17 14:17:34 -0500

    more work

--- wrapper-extrapolating_gendiv_WM.sh
+++ wrapper-extrapolating_gendiv_WM.sh
@@ -6,6 +6,8 @@ executable=run-extrapolating_gendiv_WM.sbatch #script to run
 
 logfilesdir=logfiles_gendivextrap_WM #name of directory to create and then write log files to
 
+storagenode=null #place holder bc we aren't currently using this variable
+
 cpus=2 #number of CPUs to request/use per dataset
 ram_per_cpu=8G #amount of RAM to request/use per CPU
 time=168:00:00
@@ -36,8 +38,8 @@ do
 	#define path to where downloaded raw/unprocessed sequence data live for this dataset
 	#and where clean/processed reads should be written to
 	indir=empirical_data/$run_name
-	outdir=/mnt/scratch/$USER/$run_name/gendiv_data
-	figdir=/mnt/scratch/$USER/gendiv-figures
+	outdir=/mnt/scratch/$USER/ALL_WM_empirical_outputs
+	figdir=/mnt/scratch/$USER/ALL_WM_empirical_figures
 	
 	if [ ! -d $outdir ]; then mkdir -p $outdir; fi
 	if [ ! -d $figdir ]; then mkdir -p $figdir; fi
@@ -61,4 +63,4 @@ do
 	echo submitted dataset $run_name from $list_of_datasets
 	fi
 	
-done < ./$list_of_datasets
+done < ./empirical_data/$list_of_datasets
