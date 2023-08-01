611b2624722ac55093f5f4966aa0f831ba30e166
Zachary Hancock <zachbhancock@gmail.com>
2023-08-01 15:02:44 -0400

    updated

--- run_final_hpcc.r
+++ run_final_hpcc.r
@@ -21,7 +21,7 @@ print("arguments passed:")
 cat(args, sep = "\n")
 
 #define some variables (pulled in from bash script)
-workingdir = "/home/hancockz/WM_model/compiled_out"
+workingdir = "/home/hancockz/WM_model/compiled_output"
 #treefile = args[2] %>% gsub("/", "", .)
 #note to Zach for testing, run: treefile="linear_42014183_slimIter_0_sigma_0.5"
 #working dir is just the directory where "all our stuff" for one job (aka one value of sigma and one main slim iteration number) lives
