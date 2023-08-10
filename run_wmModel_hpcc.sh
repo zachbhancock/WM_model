def07daf507a239f8a95764ddd5742d1c73ecaf8
Zachary Hancock <zachbhancock@gmail.com>
2023-08-10 14:59:23 -0400

    updated

--- run_wmModel_hpcc.sh
+++ run_wmModel_hpcc.sh
@@ -5,7 +5,7 @@
 #define unique slurm jobid
 UNIQUEJOBID=${SLURM_JOB_ID}
 #define working dir on execute node
-WORKINGDIR=expcon_output
+WORKINGDIR=wm_expcon_output
 #define prefix name for outputs
 #TREEFILE="/wmModel_sigma_"$SIGMA"_K_"$K
 
@@ -14,13 +14,13 @@ if [ ! -d $WORKINGDIR ]; then mkdir $WORKINGDIR; fi
 if [ ! -d $OUTDIR ]; then mkdir $OUTDIR; fi
 
 #copy scripts to execute node
-#cp $SLURM_SUBMIT_DIR/exe_WM.R $WORKINGDIR
-#cp $SLURM_SUBMIT_DIR/wmModel_plots.R $WORKINGDIR
-#cp $SLURM_SUBMIT_DIR/wm_lib.R $WORKINGDIR
-#cp $SLURM_SUBMIT_DIR/models/wm_hom_cmpPar_mod_block_scaled.R $WORKINGDIR
-#cp $SLURM_SUBMIT_DIR/models/wm_hom_cmpPar_cmpLnL_mod_block.R $WORKINGDIR
-#cp $SLURM_SUBMIT_DIR/expcon_output/wmModel_*_sigma_"$SIGMA"_K_"$K"-pi.csv $WORKINGDIR
-#cp $SLURM_SUBMIT_DIR/expcon_output/wmModel_*_sigma_"$SIGMA"_K_"$K"-pi_locs.txt $WORKINGDIR
+cp $SLURM_SUBMIT_DIR/exe_WM.R $WORKINGDIR
+cp $SLURM_SUBMIT_DIR/wmModel_plots.R $WORKINGDIR
+cp $SLURM_SUBMIT_DIR/wm_lib.R $WORKINGDIR
+cp $SLURM_SUBMIT_DIR/models/wm_hom_cmpPar_mod_block_scaled.R $WORKINGDIR
+cp $SLURM_SUBMIT_DIR/models/wm_hom_cmpPar_cmpLnL_mod_block.R $WORKINGDIR
+cp $SLURM_SUBMIT_DIR/expcon_output/wmModel_*_sigma_"$SIGMA"_K_"$K"-pi.csv $WORKINGDIR
+cp $SLURM_SUBMIT_DIR/expcon_output/wmModel_*_sigma_"$SIGMA"_K_"$K"-pi_locs.txt $WORKINGDIR
 
 
 #move to execute node
--- wrapper-run_wmModel_hpcc.sh
+++ wrapper-run_wmModel_hpcc.sh
@@ -6,7 +6,7 @@ storagenode=slim_output #path to main node where input files live
 logfilesdir=logfiles_expcon_wmModel #name of directory to create and then write log files to
 
 datesuffix=$(date +%m-%d-%Y.%H)
-outdir=expcon_output #name of directory to create and write all outputs to
+outdir=wm_expcon_output #name of directory to create and write all outputs to
 indir=$storagenode
 finaldir=$storagenode/summary_files #where the models go to die
 
