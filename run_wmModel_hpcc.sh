7d83e3358036e95a4e8f8d57d417f70d4c7baef9
Zachary Hancock <zachbhancock@gmail.com>
2023-08-10 14:25:17 -0400

    updated:

--- run_wmModel_hpcc.sh
+++ run_wmModel_hpcc.sh
@@ -14,11 +14,11 @@ if [ ! -d $WORKINGDIR ]; then mkdir $WORKINGDIR; fi
 if [ ! -d $OUTDIR ]; then mkdir $OUTDIR; fi
 
 #copy scripts to execute node
-cp $SLURM_SUBMIT_DIR/exe_WM.R $WORKINGDIR
-cp $SLURM_SUBMIT_DIR/wmModel_plots.R $WORKINGDIR
-cp $SLURM_SUBMIT_DIR/wm_lib.R $WORKINGDIR
-cp $SLURM_SUBMIT_DIR/models/wm_hom_cmpPar_mod_block_scaled.R $WORKINGDIR
-cp $SLURM_SUBMIT_DIR/models/wm_hom_cmpPar_cmpLnL_mod_block.R $WORKINGDIR
+#cp $SLURM_SUBMIT_DIR/exe_WM.R $WORKINGDIR
+#cp $SLURM_SUBMIT_DIR/wmModel_plots.R $WORKINGDIR
+#cp $SLURM_SUBMIT_DIR/wm_lib.R $WORKINGDIR
+#cp $SLURM_SUBMIT_DIR/models/wm_hom_cmpPar_mod_block_scaled.R $WORKINGDIR
+#cp $SLURM_SUBMIT_DIR/models/wm_hom_cmpPar_cmpLnL_mod_block.R $WORKINGDIR
 #cp $SLURM_SUBMIT_DIR/expcon_output/wmModel_*_sigma_"$SIGMA"_K_"$K"-pi.csv $WORKINGDIR
 #cp $SLURM_SUBMIT_DIR/expcon_output/wmModel_*_sigma_"$SIGMA"_K_"$K"-pi_locs.txt $WORKINGDIR
 
