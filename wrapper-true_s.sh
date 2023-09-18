0b04b254012bf27a181c15f274b5c7aba165608e
Zachary Hancock <zachbhancock@gmail.com>
2023-09-18 11:49:07 -0400

    updated

--- wrapper-true_s.sh
+++ wrapper-true_s.sh
@@ -18,7 +18,7 @@ vector_of_sigma_values=( 0.5 0.75 1.0 1.25 1.5 2.0 )
 
 cpus=2 #number of CPUs to request/use per dataset
 ram_per_cpu=16G #amount of RAM to request/use per CPU
-time=120:00:00
+time=5:00:00
 
 #slurm variable key:
 # %A = SLURM_ARRAY_JOB_ID
